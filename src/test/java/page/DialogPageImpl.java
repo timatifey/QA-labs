2b3aed3908d92131fd41a3d3b3608f9aea40690c
Тимофей Android Плетнев <timofeypletnev.001@gmail.com>
2021-10-12 19:44:00 +0300

    some changes

--- src/test/java/page/DialogPageImpl.java
+++ src/test/java/page/DialogPageImpl.java
@@ -1,13 +1,12 @@
 package page;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import page.base.Page;
 import sources.Utils;
 
-import static com.codeborne.selenide.Selectors.byXpath;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DialogPageImpl extends Page implements DialogPage {
 
@@ -15,14 +14,12 @@ public class DialogPageImpl extends Page implements DialogPage {
     private final static By SEND_BTN = By.xpath(".//*[@data-tsid='button_send']");
     private final static By MSG_COLLECTION = By.xpath(".//*[@data-tsid='message_text']");
 
-
     public DialogPageImpl() {
     }
 
-
     @Override
     public void sendMessage(String msg) {
-        $(MSG_INPUT).setValue(msg);
+        Utils.sendKeys(MSG_INPUT, msg);
         Utils.click(SEND_BTN);
     }
 
@@ -39,6 +36,9 @@ public class DialogPageImpl extends Page implements DialogPage {
 
     @Override
     protected void check() {
+        sleep(3000);
+        Selenide.actions().sendKeys("11111111111111111111111111");
+        sleep(3000);
         Utils.checkPresentAndVisibility("", MSG_INPUT);
         Utils.checkPresentAndVisibility("", SEND_BTN);
     }
